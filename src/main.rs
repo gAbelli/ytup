a6c618ac337d43780fb55c7e543600f4fe7c66c2
Giorgio Abelli <giorgio.abelli99@gmail.com>
2024-05-18 16:46:09 +0200

    Add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+use clap::Parser;
 use inquire;
 
 mod client;
 mod editor;
 
+#[derive(Debug, Parser)]
+struct Args {
+    video_path: std::path::PathBuf,
+    thumbnail_path: std::path::PathBuf,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
     let youtube_client = client::YouTubeClient::new().await?;
 
     let latest_videos = youtube_client.get_last_n_videos(10).await?;
@@ -12,14 +20,24 @@ async fn main() -> anyhow::Result<()> {
         return Err(anyhow::anyhow!("No videos found"));
     }
 
-    let selected_video = inquire::Select::new("Choose a video to import data from:", latest_videos)
+    let source_video = inquire::Select::new("Choose a video to import data from:", latest_videos)
         .with_vim_mode(true)
         .prompt()?;
-    let selected_video = youtube_client.get_video_data(&selected_video.id).await?;
+    let source_video = youtube_client.get_video_data(&source_video.id).await?;
 
-    let video_upload_request = editor::create_video_upload_request(selected_video);
+    let video_upload_request = editor::create_video_upload_request(source_video)?;
 
-    println!("Video upload request: {:?}", video_upload_request);
+    println!("Uploading video...");
+    let video_id = youtube_client
+        .uplaod_video(video_upload_request, &args.video_path)
+        .await?;
+    println!("Video uploaded");
+
+    println!("Adding thumbnail...");
+    youtube_client
+        .add_thumbnail(&video_id, &args.thumbnail_path)
+        .await?;
+    println!("Thumbnail added");
 
     Ok(())
 }
