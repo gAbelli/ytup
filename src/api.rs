e7add05a8d87e0748d578973726e4b63cdc4658d
Giorgio Abelli <giorgio.abelli99@gmail.com>
2024-05-18 13:19:04 +0200

    Add serde_yaml and edit function

--- src/api.rs
+++ src/api.rs
@@ -3,6 +3,7 @@ use google_youtube3::{
     hyper, hyper::client::HttpConnector, hyper_rustls, hyper_rustls::HttpsConnector, oauth2,
     YouTube,
 };
+use serde::{Deserialize, Serialize};
 
 pub struct YouTubeApi {
     hub: YouTube<HttpsConnector<HttpConnector>>,
@@ -36,8 +37,8 @@ impl YouTubeApi {
         Ok(YouTubeApi { hub })
     }
 
-    pub async fn get_last_videos(&self, n: u32) -> Result<Vec<VideoSearchResult>> {
-        Ok(self
+    pub async fn get_last_videos(&self, n: u32) -> Result<Vec<VideoSearchResponse>> {
+        let videos: Vec<_> = self
             .hub
             .search()
             .list(&vec!["snippet".into(), "id".into()])
@@ -64,16 +65,68 @@ impl YouTubeApi {
                     .title
                     .context(format!("Could not retrieve title for video {}", video_id))?;
 
-                Ok::<VideoSearchResult, anyhow::Error>(VideoSearchResult {
+                Ok::<VideoSearchResponse, anyhow::Error>(VideoSearchResponse {
                     id: video_id,
-                    title: title,
+                    title,
                 })
             })
-            .collect())
+            .collect();
+
+        Ok(videos)
+    }
+
+    pub async fn get_video_data(&self, video_id: &str) -> Result<VideoListResponse> {
+        let video = self
+            .hub
+            .videos()
+            .list(&vec!["snippet".into()])
+            .add_id(video_id)
+            .doit()
+            .await?
+            .1
+            .items
+            .context("Could not retrieve video data from the API")?
+            .into_iter()
+            .next()
+            .context("Could not retrieve video data from the API")?;
+
+        let snippet = video.snippet.context("Could not retrieve snippet")?;
+
+        let video_data = VideoListResponse {
+            id: video_id.to_owned(),
+            title: snippet.title.context("Could not retrieve title")?,
+            description: snippet
+                .description
+                .context("Could not retrieve description")?,
+            tags: snippet.tags.context("Could not retrieve tags")?,
+            category: snippet.category_id.context("Could not retrieve category")?,
+        };
+
+        Ok(video_data)
     }
 }
 
-pub struct VideoSearchResult {
+#[derive(Debug)]
+pub struct VideoSearchResponse {
     pub id: String,
     pub title: String,
+}
+
+#[derive(Debug)]
+pub struct VideoListResponse {
+    pub id: String,
+    pub title: String,
+    pub description: String,
+    pub tags: Vec<String>,
+    pub category: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct VideoUploadRequest {
+    pub title: String,
+    pub description: String,
+    pub tags: Vec<String>,
+    pub category: String,
+    pub privacy_status: String,
+    pub publish_at: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,73 @@
 use anyhow::Result;
+use chrono::{Datelike, Days, Local, TimeZone};
 use inquire;
+use std::io::Read;
 
 mod api;
 
+pub fn edit(file_path: &str) -> Result<String> {
+    let editor = std::env::var("EDITOR")?;
+    std::process::Command::new(editor)
+        .arg(&file_path)
+        .status()?;
+    let mut content = String::new();
+    std::fs::File::open(file_path)?.read_to_string(&mut content)?;
+
+    Ok(content)
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    // let youtube_api = api::YouTubeApi::new().await?;
+    let youtube_api = api::YouTubeApi::new().await?;
     // let last_10_videos = youtube_api.get_last_videos(10).await?;
-    //
-    // let mut options: Vec<_> = last_10_videos
-    //     .iter()
-    //     .enumerate()
-    //     .map(|(i, video)| inquire::list_option::ListOption::new(i, video.title.as_str()))
-    //     .collect();
-    //
-    // options.push(inquire::list_option::ListOption::new(options.len(), "None"));
 
-    let options = vec![
-        inquire::list_option::ListOption::new(0, "Nazionali 2024 squadre 11"),
-        inquire::list_option::ListOption::new(1, "Nazionali 2024 individuali 3"),
-        inquire::list_option::ListOption::new(2, "Nazionali 2024 individuali 2"),
-    ];
+    // if last_10_videos.is_empty() {
+    //     return Err(anyhow::anyhow!("No videos found"));
+    // }
 
-    let selected = inquire::Select::new("Choose a video to import data from:", options)
-        .with_vim_mode(true)
-        .prompt()?;
+    impl std::fmt::Display for api::VideoSearchResponse {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}", self.title)
+        }
+    }
 
-    println!("Selected video: {}", selected);
+    // let selected = inquire::Select::new("Choose a video to import data from:", last_10_videos)
+    //     .with_vim_mode(true)
+    //     .prompt()?;
+
+    let video_id = "nufv6UzW8U0";
+
+    let video_data = youtube_api.get_video_data(video_id).await?;
+
+    let now = Local::now();
+    let tomorrow = Local
+        .with_ymd_and_hms(now.year(), now.month(), now.day(), 0, 0, 0)
+        .unwrap()
+        .checked_add_days(Days::new(1))
+        .unwrap()
+        .to_rfc3339();
+
+    let video_upload_request = api::VideoUploadRequest {
+        title: video_data.title,
+        description: video_data.description,
+        tags: video_data.tags,
+        category: video_data.category,
+        privacy_status: "private".to_owned(),
+        publish_at: tomorrow,
+    };
+
+    let yaml = serde_yaml::to_string(&video_upload_request)?;
+
+    let yaml_file_path = std::env::temp_dir().join("video_upload_data.yaml");
+    println!("Writing video data to {}", yaml_file_path.display());
+    std::fs::write(&yaml_file_path, yaml)?;
+
+    let yaml_file_path = yaml_file_path.to_str().unwrap();
+    let content = edit(yaml_file_path)?;
+
+    let video_upload_request: api::VideoUploadRequest = serde_yaml::from_str(&content)?;
+
+    println!("Video upload request: {:?}", video_upload_request);
 
     Ok(())
 }
